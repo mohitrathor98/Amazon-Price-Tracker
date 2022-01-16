429490552b77429caa07fdaa6fb8f252bf47d3b6
mohit <mohitrathor8@gmail.com>
2022-01-16 10:11:04 +0530

    seperated scrapper from main file

--- AmazonScrapper.py
+++ AmazonScrapper.py
@@ -1,8 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 
-response = requests.get(url="https://www.amazon.in/hz/wishlist/ls/2NQ950UEEK5LM")
-wishlist = response.text
+def get_wishlist_items(wishlist_url):
+    """Method to scrape wishlist and return list of dictionaries
+    with item name as key and links as values
+    Args:
+        wishlist_url (str): url of wishlist  from items to be fetched
+    """
+    response = requests.get(url=wishlist_url)
+    wishlist = response.text
 
-wish_parser = BeautifulSoup(wishlist, 'html.parser')
-print(wish_parser.select("h2 a"))
+    wish_parser = BeautifulSoup(wishlist, 'html.parser')    
+    contents = wish_parser.select("h2 a")
+    
+    return [{tag.getText().strip():tag.get("href")} for tag in contents] 
---
+++ main.py
@@ -0,0 +1,4 @@
+import AmazonScrapper
+
+wishlist_url = "https://www.amazon.in/hz/wishlist/ls/2NQ950UEEK5LM"
+print(AmazonScrapper.get_wishlist_items(wishlist_url))
